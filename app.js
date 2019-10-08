14d4e479bae75ce1665d34a0adef100a02ef713e
Fabulous_Rachael <rachael.whitefield@gmail.com>
2019-10-08 13:23:45 -0700

    added error message

--- app.js
+++ app.js
@@ -25,8 +25,29 @@ function calculateResults(e) {
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
         totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
     } else {
-        console.log("Please check your numbers");
+        showError("Please Check Your Numbers");
     }
 
     e.preventDefault();
+}
+
+// Show Error
+function showError(error) {
+    // Create a div
+    const errorDiv = document.createElement("div");
+
+    // Get Elements
+    const card = document.querySelector(".card");
+    const heading = document.querySelector(".heading");
+
+    // Add class
+    errorDiv.className = "alert alert-danger";
+
+    // Create text node and append to div
+    errorDiv.appendChild(document.createTextNode(error));
+
+    // Insert error above heading
+    card.insertBefore(errorDiv, heading);
+
+    
 }
